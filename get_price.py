169a405a50e873742c92d97748e23f076d0efc97
D0lores <56717078+D0lores@users.noreply.github.com>
2020-09-22 20:24:48 +0800

    Create get_price.py

---
+++ get_price.py
@@ -0,0 +1,18 @@
+
+import sys
+import  easyquotation
+
+
+
+
+if __name__ == "__main__":
+
+  stock_code=argv[1]
+	quotation = easyquotation.use('sina') 
+
+
+
+	a=quotation.real(stock_code)
+
+
+	print (a[stock_code]['now']/a[stock_code]['close'] -1)*100
